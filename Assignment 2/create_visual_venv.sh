a2beaf47b115465347f6efdccb5205ae233c7b76
Sofia Navarro <sofia.navarro@yahoo.dk>
2021-05-19 10:43:04 +0200

    update to venv scripts

--- Assignment 2/create_visual_venv.sh
+++ Assignment 2/create_visual_venv.sh
@@ -4,8 +4,13 @@ VENVNAME=ass2
 
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
+
+pip --version
 pip install --upgrade pip
 
 test -f requirements.txt && pip install -r requirements.txt
+
+# build output folder
+mkdir -p A2_output/
 
 echo "build $VENVNAME"
--- Assignment 3/create_visual_venv.sh
+++ Assignment 3/create_visual_venv.sh
@@ -4,8 +4,13 @@ VENVNAME=ass3
 
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
+
+pip --version
 pip install --upgrade pip
 
 test -f requirements.txt && pip install -r requirements.txt
+
+# build output folder
+mkdir -p A3_output/
 
 echo "build $VENVNAME"
